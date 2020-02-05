63e54afe73f8a6b9d5c6e347421c28a31a22148d
viswanathkgp12 <viswanath.iit@gmail.com>
2020-02-05 20:52:10 +0530

    add segwit address for BTC

--- main.js
+++ main.js
@@ -3,11 +3,13 @@ const bchlib = require("bitcore-lib-cash");
 const ltclib = require("litecore-lib");
 const bsvlib = require("bsv");
 
+const bitcoin = require("bitcoinjs-lib");
 
 function addressGenerator(coin, network = "regtest") {
   const p2pkhAddresses = p2pkhAddressGenerator(coin, network);
   const p2shAddresses = p2shAddressGenerator(coin, network);
-  return [
+
+  const result = [
     {
       type: "p2pkh",
       values: p2pkhAddresses
@@ -17,6 +19,19 @@ function addressGenerator(coin, network = "regtest") {
       values: p2shAddresses
     }
   ];
+  if (coin === "btc") {
+    result.push(
+      {
+        type: "p2wpkh",
+        values: p2wpkhAddressBTC(network)
+      },
+      {
+        type: "p2wsh",
+        values: p2wshAddressBTC(network)
+      }
+    );
+  }
+  return result;
 }
 
 function p2pkhAddressGenerator(coin, network = "regtest") {
@@ -33,6 +48,53 @@ function p2shAddressGenerator(coin, network = "regtest") {
   const address = new bitcore.Address(publicKeys, 2, network);
   return getPossibleAddressFormats(coin, address);
 }
+
+/**
+ **********************************************
+ * Segwit related
+ **********************************************
+ */
+function p2wpkhAddressBTC(network = "regtest") {
+  const btcNetworkObject = segwitNetworkSelection(network);
+  const keyPair = bitcoin.ECPair.makeRandom();
+  const { address } = bitcoin.payments.p2wpkh({
+    pubkey: keyPair.publicKey,
+    network: btcNetworkObject
+  });
+  return [address.toString()];
+}
+
+function p2wshAddressBTC(network = "regtest") {
+  const btcNetworkObject = segwitNetworkSelection(network);
+  const pubkeys = [];
+  for (let i = 0; i < 3; i++) {
+    const keyPair = bitcoin.ECPair.makeRandom();
+    pubkeys.push(keyPair.publicKey);
+  }
+  const { address } = bitcoin.payments.p2wsh({
+    redeem: bitcoin.payments.p2ms({ m: 3, pubkeys, network: btcNetworkObject })
+  });
+  return [address.toString()];
+}
+
+function segwitNetworkSelection(network) {
+  let btcNetworkObject;
+  switch (network) {
+    case "livenet":
+      btcNetworkObject = bitcoin.networks.bitcoin;
+      break;
+    case "testnet":
+      btcNetworkObject = bitcoin.networks.testnet;
+      break;
+    case "regtest":
+      btcNetworkObject = bitcoin.networks.regtest;
+      break;
+    default:
+      btcNetworkObject = bitcoin.networks.bitcoin;
+  }
+  return btcNetworkObject;
+}
+// End Segwit specific
 
 function getPossibleAddressFormats(coin, address) {
   switch (coin) {
@@ -79,4 +141,4 @@ function validateAddress(address, coin, network) {
 module.exports = {
   addressGenerator,
   validateAddress
-}
+};
